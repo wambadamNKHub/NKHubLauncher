63509d3bbabfc8dd76cd816ed9fdfdd71f7c8668
wambadamNKHub <wambadamagain@gmail.com>
2026-03-12 03:58:03 -0500

    GBFBR.lua

--- GBFBR.lua
+++ GBFBR.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))()
+local Rayfield = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua"))()
 
 --Vars
 local Players = game:GetService("Players")
